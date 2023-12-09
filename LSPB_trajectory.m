442ccd65a1349b4daedea19ae705798812cd01a4
lehphuc2002 <lehuuphucc2002@gmail.com>
2023-12-09 16:52:20 +0700

    test axes1 done

---
+++ LSPB_trajectory.m
@@ -0,0 +1,31 @@
+function [t, q, v, a] = LSPB_trajectory(qf, vmax, amax)
+if vmax > sqrt(qf*amax)
+    vmax = sqrt(qf*amax);
+    warndlg('Vmax is sqrt(qmax*amax), because vmax is >sqrt(qmax*max)', 'Warning');
+end
+    tc = vmax/amax;
+    qc = (amax*((tc)^2))/2;
+    qm = qf - 2*qc; 
+    tm = qm/vmax;   %t middle , tu tc --> tf-tc
+    tf = 2*tc + tm;
+    
+    t = linspace(0, tf, 100);
+    q = zeros(size(t));
+    v = zeros(size(t));
+    a = zeros(size(t));
+    for i = 1:length(t)
+        if t(i) <= tc
+            q(i) = amax*t(i)^2/2;
+            v(i) = amax*t(i);
+            a(i) = amax;
+        elseif t(i) <= tf - tc
+            q(i) = qc + vmax*(t(i) - tc);
+            v(i) = vmax;
+            a(i) = 0;
+        elseif t(i) <= tf
+            q(i) = qf - amax*(tf -t(i))^2/2;
+            v(i) = amax*(tf-t(i));
+            a(i) = -amax;
+        end
+    end
+end
